db4a67af869cb87a14ed0575e691aa91a7d0dd57
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-04-14 15:55:13 +0600

    some change have made

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
-
 const API_URL = "http://www.omdbapi.com?apikey=d944466f";
-
-const movie1 = {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
-}
 
 const App = () => {
     const [movies, setMovies] = useState([]);
